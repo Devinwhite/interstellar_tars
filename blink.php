22887c3762947619aa1aa3ccb48f191e9c22e5da
Devin White <dwhite@enhatch.com>
2019-10-08 16:41:56 -0500

    faster blink

--- blink.php
+++ blink.php
@@ -2,9 +2,9 @@
 	system("gpio -g mode 8 out");
 	
 	for ($x = 0; $x <= 50; $x++) {
-		system("sleep 5");
+		system("sleep .1");
 		system("gpio -g write 8 0");
-		system("sleep 5");
+		system("sleep .1");
 		system("gpio -g write 8 1");
 	}
 	
