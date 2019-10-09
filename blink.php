7fa95669b03f9ced06c1aa25b0ff23eb756bd35e
Devin White <dwhite@enhatch.com>
2019-10-09 08:01:59 -0500

    finish on off

--- blink.php
+++ blink.php
@@ -1,11 +1,11 @@
 <?php
 	system("gpio -g mode 8 out");
 	
-	for ($x = 0; $x <= 49; $x++) {
-		system("sleep .1");
-		system("gpio -g write 8 0");
+	for ($x = 0; $x <= 50; $x++) {
 		system("sleep .1");
 		system("gpio -g write 8 1");
+		system("sleep .1");
+		system("gpio -g write 8 0");
 	}
 	
 ?>
