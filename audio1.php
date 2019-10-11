c7bdb3aff960ee0332e0f347b231cc42aa7410e8
Devin White <dwhite@enhatch.com>
2019-10-11 08:28:38 -0500

    increase volume a bit

--- audio1.php
+++ audio1.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type: application/json');
-	system("sudo -b /usr/bin/mpg123 -g 100 mp3/TARS_1.mp3")
+	system("sudo -b /usr/bin/mpg123 -g 300 mp3/TARS_1.mp3")
 ?>
 { 
    "succcess":true
