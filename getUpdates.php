fe170ec92f0d286a07d682112fa5361179e143a2
Devin White <dwhite@enhatch.com>
2020-02-27 10:14:58 -0600

    add pull command

--- getUpdates.php
+++ getUpdates.php
@@ -1,7 +1,7 @@
 <?php
 	header('Content-Type: application/json');
 
-    $output = shell_exec('sudo git reset --hard origin/master; sudo git clean -f -d');
+    $output = shell_exec('sudo git reset --hard origin/master; sudo git clean -f -d; sudo git pull');
     $response = array( 
         "response"=>$output); 
 
